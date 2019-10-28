2c21f10a38b702d31b163de184be65f7fae81e4a
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-28 13:17:55 -0500

    completed chapter

--- src/scripts/address.js
+++ src/scripts/address.js
@@ -1,1 +1,23 @@
-console.log("Totally connected!")
+// Add an event listener to the Save button, and start collecting the value of each of the input fields.
+
+// Some starter code
+const container = document.querySelector("#addressList")
+
+document.querySelector("#saveEntry").addEventListener("click", event => {
+    /*
+        Collect the user input by selecting the input fields, one
+        at a time, and accessing the `value` property
+    */
+    const personName = document.querySelector("#fullName").value
+    const personAddress = document.querySelector("#address").value
+
+    // Once you have collected all the values, update the DOM
+    // with some HTML
+    container.innerHTML += `
+        <section>
+            <h1>${personName}</h1>
+            <div>${personAddress}</div>
+        </section>
+    `
+
+})
